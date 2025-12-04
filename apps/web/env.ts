3696481671d892204e53ad6880a1254cf9e5b242
Anirudh Kamath <github@kamath.io>
2025-12-03 21:51:34 -0500

    add posthog nextjs

--- apps/web/env.ts
+++ apps/web/env.ts
@@ -1,9 +1,15 @@
 const BRAINTRUST_API_KEY = process.env.BRAINTRUST_API_KEY;
+const NEXT_PUBLIC_POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY;
 
 if (!BRAINTRUST_API_KEY) {
   throw new Error("BRAINTRUST_API_KEY is not set");
 }
 
+if (!NEXT_PUBLIC_POSTHOG_KEY) {
+  throw new Error("NEXT_PUBLIC_POSTHOG_KEY is not set");
+}
+
 export const env = {
   BRAINTRUST_API_KEY,
+  NEXT_PUBLIC_POSTHOG_KEY,
 };
---
+++ apps/web/instrumentation-client.ts
@@ -0,0 +1,10 @@
+import posthog from "posthog-js"
+
+// Initialize PostHog client with recommended defaults
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: "/ingest",
+  ui_host: "https://us.posthog.com",
+  defaults: '2025-05-24',
+  capture_exceptions: true, // enables capturing exceptions using Error Tracking
+  debug: process.env.NODE_ENV === "development",
+});
